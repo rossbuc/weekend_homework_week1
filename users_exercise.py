f13a1e5b0ef097b18db7e4ab55a2da85471c61e2
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-07 12:24:00 +0100

    Finish task 7 in users_exercise

--- users_exercise.py
+++ users_exercise.py
@@ -89,6 +89,8 @@ for number in avril_lottery_nums:
 #   even_numbers.append(number)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"]["lottery_numbers"].append(7)
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
