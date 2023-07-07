324496f4a7a5ce6ae729d91ec008f4af70bc8cbe
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-07 12:00:16 +0100

    Finish task 4 in users_exercise

--- users_exercise.py
+++ users_exercise.py
@@ -68,6 +68,8 @@ erik_hometown = users["Erik"]["home_town"]
 erik_lottery_nums = users["Erik"]["lottery_numbers"]
 
 # 4. Get the species of Avril's pet Monty
+monty_species = users["Avril"]["pets"][0]["species"]
+
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
