e03df5c5c93992d8a9473b056cddcbb559a9a4d7
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-07 14:16:53 +0100

    Finish task 4 in advdnaced_logic_exercise and altered task 2 to create seperate sorted numbers list

--- advanced_logic_exercise.py
+++ advanced_logic_exercise.py
@@ -9,22 +9,36 @@ for number in filterfalse(lambda x : x % 2, numbers):
     even_numbers.append(number)
 
 # 2. Print the difference between the largest and smallest value:
-numbers.sort()
-difference_between = numbers[len(numbers) - 1] - numbers[0]
+sorted_numbers = sorted(numbers)
+difference_between = sorted_numbers[len(numbers) - 1] - numbers[0]
 
+print(difference_between)
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
 double_2 = False
 for x in range(0, len(numbers) - 1):
     if numbers[x] == 2 and numbers[x + 1] == 2:
         double_2 = True
 
-print(double_2)
-        
+
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
+sum_not_6_to_7 = 0
+counter = False
+
+for number in numbers:
+    if number == 6:
+        counter = True
+    elif number == 7 and counter == True:
+        counter = False
+        continue
+
+    if counter == True:
+        continue
+    else:
+        sum_not_6_to_7 += number
 
 
 # 5. HARD! Print the sum of the numbers. 
